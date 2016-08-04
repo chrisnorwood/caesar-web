d2f1b609f513f4f2b766929afd9acea95174599c
Chris Norwood <cnorwood92@gmail.com>
2016-08-03 20:47:41 -0700

    Basic cypher functionality implemented with AJAX

--- caesar_web.rb
+++ caesar_web.rb
@@ -1,6 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+def caesar_cipher string, shift = 3
+  # Create array of charset arrays to be shifted
+  alphabet  = [Array('A'..'Z'), 
+               Array('a'..'z')]
+
+  # empty encryption key
+  cipher_key = {}
+
+  # propogates cipher_key hash with pairs for each conversion
+  # left-shift [-shift] default
+  alphabet.each do |charset|
+    cipher_key.merge! Hash[charset.zip(charset.rotate(-shift))]
+  end
+
+  # iterate through characters of input and match to value found in cipher_key
+  output = string.chars.map do |char|
+    # returns char as default if no match found
+    cipher_key.fetch(char, char)
+  end
+
+  output.join
+end
+
 get '/' do
   erb :index
+end
+
+post '/cipher' do
+  text = params[:text]
+  
+  caesar_cipher(text)
 end
